9500ee8a64c883a5878f937f7446962994a30244
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-21 21:32:43 -0700

    finished readme

--- src/App.js
+++ src/App.js
@@ -36,47 +36,41 @@ function App() {
   const sortEmployeeName = () => {
     const sortedEmployeeName = [...employees]
     sortedEmployeeName.sort((a, b) => {
-      var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
-      var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+      var nameA = a.name.first.toUpperCase(); 
+      var nameB = b.name.first.toUpperCase(); 
       if (nameA < nameB) {
         return -1;
       }
       if (nameA > nameB) {
         return 1;
       }
-      // names must be equal
       return 0;
-      
     });
     if (isAscending) {
       sortedEmployeeName.sort((a, b) => {
-        var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
-        var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+        var nameA = a.name.first.toUpperCase();
+        var nameB = b.name.first.toUpperCase();
         if (nameA < nameB) {
           return 1;
         }
         if (nameA > nameB) {
           return -1;
         }
-        // names must be equal
         return 0;
-        
       });
       setIsAscending(false);
     }
     else {
       sortedEmployeeName.sort((a, b) => {
-        var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
-        var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+        var nameA = a.name.first.toUpperCase(); 
+        var nameB = b.name.first.toUpperCase();
         if (nameA < nameB) {
           return -1;
         }
         if (nameA > nameB) {
           return 1;
         }
-        // names must be equal
         return 0;
-        
       });
       setIsAscending(true);
     }
